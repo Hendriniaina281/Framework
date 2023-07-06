50c5c0a7bc8c6ab71d069e3f589ee4e2ac145b24
Hendry2801 <hendriniainaliantsoa@gmail.com>
2023-07-06 15:01:48 +0300

    Sprint2

--- src/java/etu1760/framework/servlet/FrontServlet.java
+++ src/java/etu1760/framework/servlet/FrontServlet.java
@@ -5,18 +5,21 @@
  */
 package etu1760.framework.servlet;
 
+import etu.framework.Mapping;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 /**
  *
  * @author P15A-13-Hendry
  */
 public class FrontServlet extends HttpServlet {
+    HashMap<String,Mapping> mappingUrls;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
